226d0eb7e90a88929a8341b71b742f5345d35fc1
Siju Sakaria <sijus887@gmail.com>
2022-10-15 13:01:37 -0400

    Fetch emails only for specific addresses

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,7 @@ from googleapiclient.discovery import build
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.modify']
 
+companies = list
 
 def readEmails():
     """Shows basic usage of the Gmail API.
@@ -38,51 +39,81 @@ def readEmails():
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
     try:
-        # Call the Gmail API
-        service = build('gmail', 'v1', credentials=creds)
-        results = service.users().messages().list(userId='me', labelIds=['INBOX'], q="is:read").execute()
-        messages = results.get('messages', [])
-        if not messages:
-            print('No new messages.')
-        else:
-            message_count = 0
-            for message in messages:
-                message_count = message_count + 1
-                # TODO Remove this
-                if message_count == 200:
-                    break
+        for company in companies:
+            # Call the Gmail API
+            service = build('gmail', 'v1', credentials=creds)
+            name = f"{company}"
+            print(name)
+            results = service.users().messages().list(userId='me', labelIds=['INBOX'], q=f"from:{name}").execute()
+            messages = results.get('messages', [])
+            print ("Count of Messages from ", name , " is ", len(messages))
+            if not messages:
+                print('No new messages.')
+            else:
+                for message in messages:
+                    msg = service.users().messages().get(userId='me', id=message['id']).execute()
+                    email_data = msg['payload']['headers']
+                    for values in email_data:
+                        name = values['name']
+                        if name == 'From':
+                            from_name = values['value']
+                            # if is_not_job_email(from_name):
+                            #     continue
+                            payload = msg['payload']
+                            parts = payload.get('parts')
 
-                msg = service.users().messages().get(userId='me', id=message['id']).execute()
-                email_data = msg['payload']['headers']
-                for values in email_data:
-                    name = values['name']
-                    if name == 'From':
-                        from_name = values['value']
-                        print("From name:" + from_name)
-                        if from_name != get_from_address():
-                            continue
-
-                        for part in msg['payload']['parts']:
-                            try:
-                                data = part['body']["data"]
+                            if parts is None:
+                                body = payload.get("body")
+                                data = body.get("data")
                                 byte_code = base64.urlsafe_b64decode(data)
 
                                 text = byte_code.decode("utf-8")
                                 text = format_text(text)
-                                print("This is the message: " + text)
+                                print("MESSAGE: " + text)
+                                continue
 
-                                # mark the message as read (optional)
-                                msg = service.users().messages().modify(userId='me', id=message['id'],
-                                                                        body={'removeLabelIds': ['UNREAD']}).execute()
-                            except BaseException as error:
-                                pass
+                            for part in parts:
+                                try:
+                                    body = part.get("body")
+                                    data = body.get("data")
+                                    mimeType = part.get("mimeType")
+                                    # with attachment
+                                    if mimeType == 'multipart/alternative':
+                                        subparts = part.get('parts')
+                                        for p in subparts:
+                                            body = p.get("body")
+                                            data = body.get("data")
+                                            mimeType = p.get("mimeType")
+                                            if mimeType == 'text/plain':
+                                                byte_code = base64.urlsafe_b64decode(data)
+                                                break
+                                            elif mimeType == 'text/html':
+                                                byte_code = base64.urlsafe_b64decode(data)
+                                                break
+                                        # without attachment
+                                    elif mimeType == 'text/plain':
+                                        byte_code = base64.urlsafe_b64decode(data)
+                                    else:
+                                        continue
+
+                                    text = byte_code.decode("utf-8")
+                                    text = format_text(text)
+                                    print("This is the message: " + text)
+                                except BaseException as error:
+                                    print(error)
+                                    pass
     except Exception as error:
         print(f'An error occurred: {error}')
 
 
-def get_from_address():
-    return "HackHarvard <team@hackharvard.io>"
+def is_not_job_email(mail_from):
+    for company in companies:
+        result = company in mail_from
+        # print(result, "mail_from:", result, " company:", company)
+        if result:
+            return not result
 
+    return True
 
 def format_text(text):
     text = remove_html_tags(text)
@@ -99,6 +130,20 @@ def remove_extra_spaces(text):
     print(text)
     return text
 
+def get_list_of_companies():
+
+    my_file = open("companies.txt", "r")
+
+    data = my_file.read()
+
+    # replacing end splitting the text
+    # when newline ('\n') is seen.
+    data_into_list = data.split("\n")
+    print(data_into_list)
+    my_file.close()
+    return data_into_list
+
 
 if __name__ == '__main__':
+    companies = get_list_of_companies()
     readEmails()
