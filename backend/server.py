cc21bd6e716221c0e04e8452c79416510e229cfc
Sualeh <smsali97@gmail.com>
2022-10-16 00:18:54 -0400

    feat: added cors support

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
 import flask
+from flask_cors import CORS, cross_origin
 
 from main import readEmails
 from dotenv import load_dotenv
@@ -7,10 +8,12 @@ load_dotenv()
 
 
 app = flask.Flask(__name__)
+cors = CORS(app)
 app.config["DEBUG"] = True
 app.config.from_pyfile('settings.py')
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-
+@cross_origin()
 @app.route('/jobs', methods=['GET'])
 def fetch_jobs():
     return readEmails()
